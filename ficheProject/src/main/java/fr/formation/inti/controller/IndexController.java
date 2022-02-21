4cdbcb3c43c83f458946b100f4a4796584215fbf
jlbmn <johanalibman@gmail.com>
2022-02-21 09:23:11 +0100

    add links between HTML pages

--- ficheProject/src/main/java/fr/formation/inti/controller/IndexController.java
+++ ficheProject/src/main/java/fr/formation/inti/controller/IndexController.java
@@ -8,28 +8,85 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class IndexController {
 	
+	/**
+	 * acceder a la page principale du projet 
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
 	public String welcome(Model model) {
 		return "index";
 	}
 	
+	/**
+	 * (à supprimer par la suite)
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/results", method = RequestMethod.GET)
-	public String results(Model model) {
+	public String getResults(Model model) {
 		return "results";
 	}
 	
+	/**
+	 * afficher les résultats de la recherche par matière
+	 * obtenue via formulaire
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/results", method = RequestMethod.POST)
+	public String postResults(Model model) {
+		return "results";
+	}
+	
+	/**
+	 * afficher la page de la fiche sélectionnée
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/displayPdf", method = RequestMethod.GET)
 	public String displayPdf(Model model) {
 		return "displayPdf";
 	}
 	
+	/**
+	 * accéder à la page fileUpload
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
-	public String uploadFile(Model model) {
+	public String getUploadFile(Model model) {
 		return "fileUpload";
 	}
 	
+	/**
+	 * ajouter une nouvelle fiche
+	 * via formulaire 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+	public String postUploadFile(Model model) {
+		return "fileUpload";
+	}
+	
+	/**
+	 * accéder à la page login
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login(Model model) {
+	public String getLoginPage(Model model) {
+		return "login";
+	}
+	
+	/** 
+	 * vérifier la connexion
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String postCheckLogin(Model model) {
 		return "login";
 	}
 	
