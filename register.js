74bb5e788e1d4f906d656da296ab207110a09563
Eleknix <97474282+Eleknix@users.noreply.github.com>
2022-02-23 18:02:13 +0300

    Update register.js

--- register.js
+++ register.js
@@ -15,3 +15,26 @@ $(document).ready(function() {
 
     });
 });
+
+function verifyPassword() {
+    var pw = document.getElementById("confirm-pass").value;
+    //check empty password field  
+    if (confirm - pass == "") {
+        document.getElementById("message").innerHTML = "**Fill the password please!";
+        return false;
+    }
+
+    //minimum password length validation  
+    if (confirm - pass.length < 8) {
+        document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";
+        return false;
+    }
+
+    //maximum length of password validation  
+    if (confirm - pass.length > 15) {
+        document.getElementById("message").innerHTML = "**Password length must not exceed 15 characters";
+        return false;
+    } else {
+        alert("Password is correct");
+    }
+}
